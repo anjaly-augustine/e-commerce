30e6fc91d255737feca76a369bfe8d34245381fd
anjaly-augustine <anjalyaugustine35s@gmail.com>
2026-01-16 23:30:38 +0530

    feat: Add start script to package.json and log server port in app.js

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const productRoutes = require('./routes/products');
 const orderRoutes = require('./routes/orders');
 const userRoutes = require('./routes/user');
 const cartRoutes = require('./routes/cart');
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
@@ -35,3 +36,7 @@ mongoose
     );
   })
   .catch((err) => console.log(err));
+
+  app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
